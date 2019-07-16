acab509ba1a42c6c98473814097f94fe17cfb03f
Hil <hildebrando.escudero@udc.edu>
2019-07-16 01:52:04 -0400

    Begin Debug Contact me Form

--- assets/PHP/contactHandler.php
+++ assets/PHP/contactHandler.php
@@ -1,24 +1,29 @@
 <?php
-
     $name = $_POST['name'];
-    $user_email = $_POST['email'];
-    $message = $_Post['message'];
+    $email = $_POST['email'];
+    $subject = ['subject'];
+    $message = $_POST['message'];
 
-    $from = 'From: theuser';
-    $to = "thehescu6@gmail.com";
+    $from = 'From: $name';
+    $to = 'thehescu6@gmail.com';
 
-    $subject = 'New User Email';
+    $spamcheck = $_POST['spamcheck'];
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-    if ($_POST['submit'] && $human == '4') {
-          if (mail ($to, $subject, $body, $from)) {
-            echo '<p>Your message has been sent!</p>';
-    	    } else { 
-    	    echo '<p>Something went wrong, go back and try again!</p>';
+    if ($_POST['submit']) {
+        if ($name != '' && $email != '') {
+            if ($spamcheck == '10') {
+                if (mail ($to, $subject, $body, $from)) {
+    	        echo '<p>Your message has been sent!</p>';
+    	    } else {
+    	        echo '<p>Something went wrong, go back and try again!</p>';
+    	    }
+    	} else if ($_POST['submit'] && $spamcheck != '10') {
+    	    echo '<p>It seems you could be a bot...do it again</p>';
     	}
-    } else if ($_POST['submit'] && $human != '4') {
-    	echo '<p>You answered the anti-spam question incorrectly!</p>';
+        } else {
+            echo '<p>You are missing some info.</p>';
         }
-
+    }
 ?>
